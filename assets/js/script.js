22ce42b0c65a8a76985be9a417d5e1235f0aeca5
OmarRwiheb <59856062+OmarRwiheb@users.noreply.github.com>
2024-05-16 03:28:18 +0300

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,45 @@
 let hamburger = document.querySelector(".hamburger");
 let mob = document.querySelector(".mobile-nav");
 hamburger.addEventListener("click", () => {
-    if (mob.style.display == "none") {
-        mob.style.display = "block";
-        setTimeout(() => {
-            document.querySelector(".nav-content").style.transform =
-                "translateX(100%)";
-        }, 100);
-    } else {
-        setTimeout(() => {
-            document.querySelector(".nav-content").style.transform =
-                "translateX(-100%)";
-        }, 100);
-        mob.style.display = "none";
-    }
-});
-document.querySelector(".nav-blocker").addEventListener("click", () => {
+  if (mob.style.display == "none") {
+    mob.style.display = "block";
     setTimeout(() => {
-        document.querySelector(".nav-content").style.transform =
-            "translateX(-100%)";
+      document.querySelector(".nav-content").style.transform =
+        "translateX(100%)";
+    }, 100);
+  } else {
+    setTimeout(() => {
+      document.querySelector(".nav-content").style.transform =
+        "translateX(-100%)";
     }, 100);
     mob.style.display = "none";
+  }
+});
+document.querySelector(".nav-blocker").addEventListener("click", () => {
+  setTimeout(() => {
+    document.querySelector(".nav-content").style.transform =
+      "translateX(-100%)";
+  }, 100);
+  mob.style.display = "none";
+});
+
+let dropdownProfile = document.querySelector(".user-list");
+let dropdownProfileList = document.getElementById("dropdown-profile");
+let dropdownArrow = document.querySelector(".arrow");
+
+dropdownProfile.addEventListener("click", () => {
+  if (dropdownProfileList.style.display == "none") {
+    dropdownProfileList.style.display = "flex";
+  } else {
+    dropdownProfileList.style.display = "none";
+  }
+});
+
+let mobNav = document.querySelector("#dropdown-head-mob");
+let mobNav_menu = document.getElementById("mob-dropdown");
+let cover = document.querySelector(".list-cover");
+mobNav.addEventListener("click", () => {
+  if (cover.style.transform == "translateY(1px)")
+    cover.style.transform = "translateY(120px)";
+  else cover.style.transform = "translateY(1px)";
 });
